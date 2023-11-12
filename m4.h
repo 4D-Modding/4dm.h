033b4e3557d355e463c7a9262107111a8862897a
Tr1NgleDev <77019520+Tr1NgleDev@users.noreply.github.com>
2023-11-12 22:25:52 +0800

    added another Mat5 from Rotor

--- m4.h
+++ m4.h
@@ -139,6 +139,33 @@ namespace fdm
 					FUNC_M4_MAT5_MAT5_A
 					)(this, j);
 			}
+			Mat5(const Rotor& rotor)
+			{
+				memset(value, 0, 5 * 5 * sizeof(float));
+			
+				glm::vec4 x = rotor.rotate({ 1, 0, 0, 0 });
+				value[0][0] = x[0];
+				value[0][1] = x[1];
+				value[0][2] = x[2];
+				value[0][3] = x[3];
+				glm::vec4 y = rotor.rotate({ 0, 1, 0, 0 });
+				value[1][0] = y[0];
+				value[1][1] = y[1];
+				value[1][2] = y[2];
+				value[1][3] = y[3];
+				glm::vec4 z = rotor.rotate({ 0, 0, 1, 0 });
+				value[2][0] = z[0];
+				value[2][1] = z[1];
+				value[2][2] = z[2];
+				value[2][3] = z[3];
+				glm::vec4 w = rotor.rotate({ 0, 0, 0, 1 });
+				value[3][0] = w[0];
+				value[3][1] = w[1];
+				value[3][2] = w[2];
+				value[3][3] = w[3];
+			
+				value[4][4] = 1.0;
+			}
 			nlohmann::json toJson()
 			{
 				nlohmann::json result{};
