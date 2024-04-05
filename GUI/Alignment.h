910018bf7b9c9ad6696f6193b9a0135fe0b69adc
Redjard <github@redjard.org>
2024-04-05 19:33:52 +0200

    Fix improper enum definitions

--- GUI/Alignment.h
+++ GUI/Alignment.h
@@ -6,13 +6,13 @@ namespace fdm
 {
 	namespace gui
 	{
-		enum AlignmentX 
+		enum AlignmentX: int
 		{
 			ALIGN_LEFT = 0,
 			ALIGN_RIGHT = 1,
 			ALIGN_CENTER_X = 2
 		};
-		enum AlignmentY
+		enum AlignmentY: int
 		{
 			ALIGN_TOP = 0,
 			ALIGN_BOTTOM = 1,
