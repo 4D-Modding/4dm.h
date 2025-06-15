394ca0b0956a0cc17b7e46c5cab3da9662b095c0
Tr1NgleDev <77019520+Tr1NgleDev@users.noreply.github.com>
2025-06-16 05:27:51 +0800

    help

--- Connection/InMessage.h
+++ Connection/InMessage.h
@@ -13,15 +13,15 @@ namespace fdm::Connection
 			reinterpret_cast<void(__fastcall*)(SteamNetworkingMessage_t * message)>(getFuncAddr((int)Func::Connection::InMessage::InMessageDeleter))(message);
 		}
 
-		std::unique_ptr<SteamNetworkingMessage_t, void(__cdecl*)(SteamNetworkingMessage_t*)> messagePtr{ nullptr, InMessageDeleter };
-		InMessage() : messagePtr(nullptr, InMessageDeleter) {}
-		InMessage(SteamNetworkingMessage_t* msg) : messagePtr(msg, InMessageDeleter) {}
+		std::unique_ptr<SteamNetworkingMessage_t, decltype(&InMessageDeleter)> messagePtr{ nullptr, &InMessageDeleter };
+		InMessage() {}
+		InMessage(SteamNetworkingMessage_t* msg) : messagePtr(msg, &InMessageDeleter) {}
 
 		size_t getDataSize() const { return messagePtr->m_cbSize; }
 		size_t getMsgSize() const { return messagePtr->m_cbSize - sizeof(uint16_t); }
 		const char* getData() const { return (char*)messagePtr->m_pData; }
 		const char* getMsgData() const { return (char*)messagePtr->m_pData + sizeof(uint16_t); }
-		stl::string getStrData() const { return  stl::string((char*)messagePtr->m_pData + sizeof(uint16_t), getMsgSize()); }
+		stl::string getStrData() const { return  stl::string(getMsgData(), getMsgSize()); }
 		uint16_t getPacketType() const { return *(uint16_t*)messagePtr->m_pData; }
 		uint32_t getClient() const { return messagePtr->m_conn; }
 	};
