f480bd1f6ee01bd0c367f36a6c548102505572ab
Tr1NgleDev <tr1ngle@tr1ngle.dev>
2025-06-16 01:29:44 +0800

    fix OutMessage memory issues
    thanks to @Mashpoe :)
    before this fix there was a chance for random packets to merge into one, in most cases crashing the game.

--- Connection/OutMessage.h
+++ Connection/OutMessage.h
@@ -14,54 +14,49 @@ namespace fdm::Connection
 	public:
 		struct HeapMessageData
 		{
-			std::atomic<int> refCount; // 0x0
+			std::atomic<int> refCount = 0; // 0x0
 			Connection::MessageData data; // 0x4
 		};
+		inline static void ReleaseHeapMessageData(Connection::MessageData* data)
+		{
+			reinterpret_cast<void(__fastcall*)(Connection::MessageData * data)>(getFuncAddr((int)Func::Connection::OutMessage::ReleaseHeapMessageData))(data);
+		}
+		inline static MessageData* CreateHeapMessageData(size_t contentSize)
+		{
+			HeapMessageData* heapMessageData = (HeapMessageData*)malloc(sizeof(HeapMessageData) + contentSize);
+			heapMessageData->refCount = 1;
+			return &heapMessageData->data;
+		}
 
 		inline static void FreeSteamNetworkingMessagePayload(SteamNetworkingMessage_t* pMsg)
 		{
 			return reinterpret_cast<void(__fastcall*)(SteamNetworkingMessage_t*)>(getFuncAddr((int)Func::Connection::OutMessage::FreeSteamNetworkingMessagePayload))(pMsg);
 		}
-
-		inline static void ReleaseHeapMessageData(Connection::MessageData* data)
-		{
-			reinterpret_cast<void(__fastcall*)(Connection::MessageData * data)>(getFuncAddr((int)Func::Connection::OutMessage::ReleaseHeapMessageData))(data);
-		}
-
 		SteamNetworkingMessage_t* createMessage(uint32_t recipient, int sendFlags = k_nSteamNetworkingSend_Reliable) const
 		{
 			return reinterpret_cast<SteamNetworkingMessage_t * (__thiscall*)(const Connection::OutMessage*, unsigned int, int)>(getFuncAddr((int)Func::Connection::OutMessage::createMessage))(this, recipient, sendFlags);
 		}
 
-		std::unique_ptr<MessageData, void(__cdecl*)(MessageData*)> data{ nullptr, ReleaseHeapMessageData };
+		using DataOwner = std::unique_ptr<MessageData, decltype(&ReleaseHeapMessageData)>;
+		DataOwner data{ nullptr, &ReleaseHeapMessageData };
 		uint64_t dataSize = 0;
 
-		void init(uint16_t type, void* data = nullptr, uint64_t dataSize = 0)
+		OutMessage() { }
+		OutMessage(uint16_t type, size_t contentSize) :
+			dataSize(sizeof(MessageData) + contentSize),
+			data(DataOwner{ CreateHeapMessageData(contentSize), &ReleaseHeapMessageData })
 		{
-			HeapMessageData* msg = (HeapMessageData*)malloc(sizeof(HeapMessageData) + dataSize);
-
-			if (msg == nullptr) return;
-
-			msg->refCount++;
-			msg->data.type = type;
-
-			if (data != nullptr)
-				memcpy(&msg->data.content, data, dataSize);
-
-			this->data = std::unique_ptr<MessageData, void(__cdecl*)(MessageData*)>(&msg->data, ReleaseHeapMessageData);
-			this->dataSize = dataSize + 2;
+			data->type = type;
 		}
-
-		OutMessage() : data(nullptr, ReleaseHeapMessageData) {}
-		OutMessage(Connection::MessageData* msgData) : data(msgData, ReleaseHeapMessageData) {}
-		OutMessage(uint16_t type, void* data = nullptr, uint64_t dataSize = 0)
+		OutMessage(uint16_t type, void* data = nullptr, size_t dataSize = 0) :
+			OutMessage(type, dataSize)
 		{
-			init(type, data, dataSize);
+			if (data)
+			{
+				std::memcpy(this->data->content, data, dataSize);
+			}
 		}
-		OutMessage(Packet::ClientPacket type, void* data = nullptr, uint64_t dataSize = 0) : OutMessage((uint16_t)type, data, dataSize) {}
-		OutMessage(Packet::ServerPacket type, void* data = nullptr, uint64_t dataSize = 0) : OutMessage((uint16_t)type, data, dataSize) {}
-
-		OutMessage(Packet::ClientPacket type, const stl::string& str) : OutMessage((uint16_t)type, (void*)str.c_str(), str.size()) {}
-		OutMessage(Packet::ServerPacket type, const stl::string& str) : OutMessage((uint16_t)type, (void*)str.c_str(), str.size()) {}
+		OutMessage(uint16_t type, const stl::string& str) :
+			OutMessage(type, (void*)str.c_str(), str.size()) { }
 	};
 }
